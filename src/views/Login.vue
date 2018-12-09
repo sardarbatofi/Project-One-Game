37c9f3a454b4db76ffddb1e4e1b38af14ae87709
Jimmy Svensson <jimmy888swe@gmail.com>
2018-12-09 22:51:38 +0100

    loginAndFirebase

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login">
+      <h2>Logga in</h2>
+     <p>E-Post: </p> <input type="text" placeholder="exempel@onegame.se"><br>
+      <p>Lösenord:  </p><input type="text" placeholder="******"><br>
+      <button @click="login">Logga in</button><br>
+      <p>Behöver du ett konto? <router-link to="/sign-up"> Klicka här </router-link> </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  data() {
+    return {};
+  },
+  methods:{
+      login: function(){
+          this.$router.replace('home');
+      }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+    margin: 5% 0 3% 0%;
+}
+.login{
+  margin: 0 10%;
+  padding: 1%;
+  border-radius: 5px;
+  background-color: #cccccc;
+}
+p{
+    display: inline-block;
+    padding:0 5px 0 0;
+    font-size: 1em;
+
+}
+button, a {
+cursor: pointer;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sign-up">
+      <h2>Skapa ett nytt konto.</h2><br>
+     <p>E-Post: </p> <input type="text" placeholder="exempel@onegame.se"><br>
+      <p>Lösenord:  </p><input type="text" placeholder="******"><br>
+      <button>Let´s do it!</button><br>
+      <p>Tillbaka?, <router-link to="/login">Klicka här</router-link></p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'signUp',
+  data() {
+    return {};
+  },
+  methods:{}
+}
+</script>
+
+<style scoped>
+h2 {
+    margin: 5% 0 3% 0%;
+}
+.sign-up{
+  margin: 0 10%;
+  padding: 1%;
+  border-radius: 5px;
+  background-color: #cccccc;
+}
+p{
+    display: inline-block;
+    padding:0 5px 0 0;
+    font-size: 1em;
+    margin: 2%
+}
+button, a {
+cursor: pointer;
+}
+</style>
