58a3a6ee9bcc7ba16c703a8cd1371baa7b6a3b5a
reeklof <reeklof@live.com>
2018-12-11 14:34:28 +0100

    change file from index to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <input v-model="newNumber" type="number"><br>
+    <button @click='addNumber'>Enter number</button> 
   </div>
 </template>
