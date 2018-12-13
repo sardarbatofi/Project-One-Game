6ad95de7e9e741cf4649125f00f6aafbba863ac4
Jimmy Svensson <jimmy888swe@gmail.com>
2018-12-13 09:28:44 +0100

    RenamedComp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="pepparkakshus" src="../assets/pepparkakshus.jpg">
-    <HelloWorld />
+    <Game />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Game from '@/components/Game.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Game
   }
 }
 </script>
