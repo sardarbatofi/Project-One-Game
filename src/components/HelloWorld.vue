3cb974ecb56859ca197f712fcedb9088764b49a3
Jimmy Svensson <jimmy888swe@gmail.com>
2018-12-13 07:17:18 +0100

    beginingOfGame

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,90 @@
+
 <template>
-  <div class="hello">
-   
+    <div id="app">
+      <p>We have selected a random number between 1 and 100. See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</p>
+  
+    <button @click="startGame" v-on:click="game = !game"> Start Game</button><br>
+  <div v-show="game">
+  <input v-model="userGuess" type="number" @keypress.enter = "userInput" :disabled="inputClosed"/>
+  <button @click="userInput" :disabled="inputBtnClosed">Guess</button>
+  
+  <p>Rick: {{opponent}}</p>
+  <p>Marty: {{opponent2}}</p>
+
+  <br>
+  <p>You have uesed {{numberOfGuess}} of 10 Guesses.</p>
+  
+  <p>{{history}}</p>
+</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+   data() {
+     return{
+  opponent: 0,
+  opponent2: 0,
+  game: false,
+  inputClosed: false,
+  inputBtnClosed: false,
+  numberOfGuess: 0,
+  number:0,
+  userGuess:0,
+  history: [] 
+     }
+},
+      methods: {
+      startGame: function(){
+          this.opponent= 0,
+          this.opponent2= 0,
+          this.game= false,
+          this.inputClosed= false,
+          this.inputBtnClosed= false,
+          this.numberOfGuess= 0,
+          this.number=0,
+          this.userGuess =0,
+          this.history= []
+          this.number = Math.floor(Math.random() * 100)+1;
+      },
+      userInput: function(userGuess, number) {
+        if(this.userGuess < this.number /*&& numberOfGuess < 11 */){
+          alert("Wrong, guess higher!")
+          this.opponent +=7;
+          this.opponent2 +=19;
+          this.numberOfGuess++;
+          this.history.push('userGuess')
+          this.history.push('opponent')
+          this.history.push('opponent2')
+          
+        }
+        else if (this.userGuess > this.number /*&& numberOfGuess < 11 */){
+          alert("Wrong, guess lower!")
+          this.opponent -= 1;
+          this.opponent2 += 11;
+          this.numberOfGuess++;
+          this.history.push('userGuess')
+          this.history.push('opponent')
+          this.history.push('opponent2')
+        }
+        else if (this.userGuess == this.number /*&& numberOfGuess < 11 */){
+          alert("Yes sir, The Answere is " + this.number);
+          this.numberOfGuess++;
+          this.inputClosed = true
+          this.inputBtnClosed = true 
+          
+        }
+        else /*if (numberOfGuess >= 10)*/{
+          alert('You have uesd all your guesses, GAME OVER');
+       
+          this.inputClosed = true
+          this.inputBtnClosed = true 
+        }
+      }
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="pepparkakshus" src="../assets/pepparkakshus.jpg">
+    <HelloWorld />
   </div>
 </template>
 
@@ -16,3 +16,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+img{
+ width: 200px; 
+}
+</style>
